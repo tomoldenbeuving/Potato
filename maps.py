db50355d65babf3103637a5572677a040db41acb
joepapi2009 <piervdp@gmail.com>
2021-12-08 13:14:40 +0100

    ja

--- maps.py
+++ maps.py
@@ -8,12 +8,12 @@ df = pd.read_csv("data.csv", delimiter=',', skiprows=0)
 
 #gff testen hoe github werkt
 #hieronder is alleemaal specifiek voor de kaart module
-geometry = [Point(xy) for xy in zip(df['Longitude'], df['Latitude'])]
-gdf = GeoDataFrame(df, geometry=geometry)   
+#geometry = [Point(xy) for xy in zip(df['Longitude'], df['Latitude'])]
+#gdf = GeoDataFrame(df, geometry=geometry)   
 
 #this is a simple map that goes with geopandas
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-gdf.plot(ax=world.plot(figsize=(10, 6)), marker='o', color='red', markersize=15);
+#world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+#gdf.plot(ax=world.plot(figsize=(10, 6)), marker='o', color='red', markersize=15);
 
-#    df.longitude.min(5.39442),   df.longitude.max(5.39892),      
-#   df.latitude.min(52.16532), df.latitude.max(52.16814)
+#df.longitude.min(5.39442), df.longitude.max(5.39892),      
+#df.latitude.min(52.16532), df.latitude.max(52.16814)
