ccbb6be2427834e2ccab92602a190f253b9c2350
tomoldenbeuving <tom.oldenbeuving@gmail.com>
2021-12-13 12:41:43 +0100

    netter maken

--- maps.py
+++ maps.py
@@ -1,16 +1,8 @@
 # %%
 import folium
+import pandas as pd
 # Build the default map for a specific location
 m = folium.Map(location=[52.166665, 5.398278], zoom_start=23)
-# Import the pandas library
-
-m
-
-# %%
-#%%
-# Import the pandas library
-import pandas as pd
-
 # Make a data frame with dots to show on the map
 data = pd.DataFrame({
    'lon':[-58, 2, 145, 30.32, -4.03, -73.57, 36.82, -38.5],
@@ -19,15 +11,12 @@ data = pd.DataFrame({
    'value':[10, 12, 40, 70, 23, 43, 100, 43]
 }, dtype=str)
 data 
-#%%
-#%%
 # add marker one by one on the map
 for i in range(0,len(data)):
    folium.Marker(
       location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
       popup=data.iloc[i]['name'],
    ).add_to(m)
-
 # Show the map again
 m
 #%%
