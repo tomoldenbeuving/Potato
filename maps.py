d3785b50b02213d85832146c648ec2f7d4ec6a5f
joepapi2009 <piervdp@gmail.com>
2021-12-13 12:59:17 +0100

    df1 in map

--- maps.py
+++ maps.py
@@ -4,18 +4,14 @@ import pandas as pd
 # Build the default map for a specific location
 m = folium.Map(location=[52.166665, 5.398278], zoom_start=23)
 # Make a data frame with dots to show on the map
-data = pd.DataFrame({
-   'lon':[-58, 2, 145, 30.32, -4.03, -73.57, 36.82, -38.5],
-   'lat':[-34, 49, -38, 59.93, 5.33, 45.52, -1.29, -12.97],
-   'name':['Buenos Aires', 'Paris', 'melbourne', 'St Petersbourg', 'Abidjan', 'Montreal', 'Nairobi', 'Salvador'],
-   'value':[10, 12, 40, 70, 23, 43, 100, 43]
-}, dtype=str)
-data 
+df1 = pd.read_csv("data.csv")
+df2 = pd.read_csv("data2.csv")
+df1
 # add marker one by one on the map
-for i in range(0,len(data)):
+for i in range(0,len(df1)):
    folium.Marker(
-      location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
-      popup=data.iloc[i]['name'],
+      location=[df1.iloc[i]['lat'], df1.iloc[i]['lon']],
+      popup=df1.iloc[i]['name'],
    ).add_to(m)
 # Show the map again
 m
