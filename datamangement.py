2a85dd8ff2c0da68e9a3a30ae0495fc4eefda94a
tomoldenbeuving <tom.oldenbeuving@gmail.com>
2022-01-06 17:02:03 +0100

    data management en kaart af

--- datamangement.py
+++ datamangement.py
@@ -5,13 +5,15 @@ import time
 
 
 def merge_data(data1, data2, sorteren_op): 
+        #opent bestand wat nog gespecificeerd moet worden en sorteert de rijen op de gespecificeerde kolom
         df1 = pd.read_csv(data1)
         df1 = df1.sort_values(by=sorteren_op)      
         
         df2 = pd.read_csv(data2)
         df2 = df2.sort_values(by=sorteren_op)
 
+        #voegd de twee datasets samen op dichtsbijzijnde absolute waarde
         df3 = pd.merge_asof(df1, df2,on="time", direction="nearest")
- #       df3.to_csv(path_or_buf="data_destination.csv", index=False)
+        #functie geeft de samengevoegde set terug
         return df3
 
