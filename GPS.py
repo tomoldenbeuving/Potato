fd589d3977e1ea4b524fbf773448e4503714c05c
tomoldenbeuving <tom.oldenbeuving@gmail.com>
2022-01-10 13:22:37 +0100

    y

--- GPS.py
+++ GPS.py
@@ -1,8 +1,13 @@
 import serial
+import pandas as pd
 #run de volgende line om een lijst met poorten te krijgen
 #python -m serial.tools.list_ports
+
+df = pd.read_csv('data.csv')
+
 from pynmeagps import NMEAReader
 stream = serial.Serial('COM3', 9600, timeout=3)
 nmr = NMEAReader(stream)
 (raw_data, parsed_data) = nmr.read()
-print(parsed_data)
+
+df.to_csv(parsed_data, index=False)
