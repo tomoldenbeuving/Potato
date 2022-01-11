621b6e2fb81ebe30cdf4dca66d7c1d290032ac61
TomDaan <tom.oldenbeuving@gmail.com>
2022-01-11 16:58:22 +0100

    nu met class

--- GPS.py
+++ GPS.py
@@ -1,14 +1,27 @@
 import serial
-import pandas as pd
 #run de volgende line om een lijst met poorten te krijgen
 #python -m serial.tools.list_ports
-
-df = pd.read_csv('data.csv')
-
 from pynmeagps import NMEAReader
-stream = serial.Serial('COM4',4800, timeout=3)
-nmr = NMEAReader(stream)
-(raw_data, parsed_data) = nmr.read()
-print(parsed_data.lat ,"," ,parsed_data.lon ,"," ,parsed_data.time)
 
-stream.close()
+
+
+class loc:
+    stream = serial.Serial('COM4',4800, timeout=3)
+    nmr = NMEAReader(stream)
+
+    global parsed_data
+    (raw_data, parsed_data) = nmr.read()
+    
+    
+    def lon():
+        print(parsed_data.lon ,",")
+    
+    def lat():
+        print(parsed_data.lat ,",")
+
+    def time():
+        print(parsed_data.time ,",")
+    
+    stream.close()
+
+
